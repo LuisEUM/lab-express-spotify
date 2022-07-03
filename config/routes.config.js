b554ce2c24d41f856edd0096639e099274e5e2e0
LuisEUM <luiseum95@gmail.com>
2022-07-04 00:57:45 +0200

    All Done

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const misc = require("../controllers/misc.controller");
+const misc = require("../controllers/misc.controller")
 
-require("./spotify.config"); // setting the spotify-api goes here
-
+require("./spotify.config") // setting the spotify-api goes here
 const spotifyController = require('../controllers/spotify.controller')
 
 router.get("/", misc.home);
-
+router.get('/artist-search', spotifyController.search)
+router.get("/albums/:id", spotifyController.album);
+router.get("/albums/:album/:id", spotifyController.songs);
 
 module.exports = router
 
--- controllers/spotify.controller.js
+++ controllers/spotify.controller.js
@@ -1,5 +1,33 @@
-const SpotifyWebApi = require('../config/spotify.config');
+const spotifyApi = require('../config/spotify.config');
 
-module.exports.home = (req, res, next) => {
+module.exports.search = (req, res, next) => {
+    spotifyApi
+    .searchArtists(req.query.search)
+    .then((data) => {
+        res.render('artist-search-results', {artists:data.body.artists.items})
+    })
+    .catch((err) =>
+        console.log("The error while searching artists occurred: ", err)
+    );
+};
 
+module.exports.album = (req, res, next) => {
+    spotifyApi.getArtistAlbums(req.params.id)
+    .then((data)=>{
+        res.render('albums', {albums:data.body.items})
+    })
+    .catch((err) =>
+        console.log("The error while searching artists occurred: ", err)
+    );
+};
+
+
+module.exports.songs = (req, res, next) => {
+    spotifyApi.getAlbumTracks(req.params.id, { limit : 20, offset : 0 })
+    .then(function(data) {
+        console.log(data.body);
+        res.render('songs', {songs:data.body.items})
+    }, function(err) {
+        console.log('Something went wrong!', err);
+    });
 };
